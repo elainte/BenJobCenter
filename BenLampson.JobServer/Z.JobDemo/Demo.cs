22805ba2171b1590caac25f53190db1c21c582aa
BenLampson(赵元源) <58603432@qq.com>
2018-07-26 09:38:39 +0800

    There's some demo code

---
+++ BenLampson.JobServer/Z.JobDemo/Demo.cs
@@ -0,0 +1,23 @@
+﻿using BenLampson.JobServer.Interface;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z.JobDemo
+{
+    public class Demo : IJobClient
+    {
+        public bool Execute()
+        {
+            using (var fw= File.CreateText($"C://{Guid.NewGuid().ToString()}.txt"))
+            {
+                fw.Write("啊啊啊啊啊啊啊啊啊啊啊");
+                fw.Close();
+            }
+            return true;
+        }
+    }
+}
